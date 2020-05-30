01e7d2f92adcab740bc8bc349c0bba24842aae27
renanmatos-hub <renan.onha@hotmail.com>
2020-05-30 13:20:56 +0000

    ajuste-30/05

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ server.use(cors());
 server.use(express.json());
 
 const pool = new Pool({
-    user: 'mwdjwugnlerish',
-    password: 'd4cb70cfcb644791928e2f5a590da4e69ba1fd6a138d49a9030900a120b7899a',
-    host: 'ec2-52-87-135-240.compute-1.amazonaws.com',
-    database: 'dbr99e9b5i24q9',
+    user: 'ukvjdbllpwdygt',
+    password: '57adeb254b05a9047ce37f8ebc8b852b725b67677d93ccd4c067d9030af098e7',
+    host: 'ec2-52-202-22-140.compute-1.amazonaws.com',
+    database: 'dfu8cv97r5n43f',
     port: 5432,
     ssl: { rejectUnauthorized: false }
 })
